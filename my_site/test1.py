ee7ca4491ad3ee9aa0554a5a39006a0378b422bd
Misha <mishagalko1@gmail.com>
2025-03-30 17:44:35 +0200

    remove not neccesary files

--- my_site/test1.py
+++
@@ -1,8 +0,0 @@
-import sqlite3 as sq
-
-db = sq.connect('C:\\Users\\Misha.DESKTOP-49T8NCT\\code files\\django\\pet project try\\my_site\\sqlite3.db')
-cur = db.cursor()
-
-a=cur.execute("SELECT * FROM test").fetchall()
-print(a)
-db.commit()
